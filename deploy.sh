890176d3090b79aa367d132288ba3c57d9896465
Ali Foroughi <ali.foroughi92@gmail.com>
2023-10-28 14:37:45 +0330

    removed deploy.sh

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=root
-HOST=141.11.182.13
-DIR=/var/www/workingtitle.pro/   # the directory where your web site files should go
-
-hugo && rsync -avz -e "ssh -p 9011" --delete public/ ${USER}@${HOST}:${DIR}
-
-exit 0
